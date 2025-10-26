dc3aae50c00e1a4be59d7ca40adb4fa3ad91b991
Timofey <50114291+Timofey-91@users.noreply.github.com>
2025-10-26 18:46:37 +0500

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Starting AceStream engine..."
-./start-engine --client-console &
-
-echo "Waiting for AceStream to start..."
-sleep 20
-
-echo "Starting nginx..."
-nginx -g "daemon off;"
